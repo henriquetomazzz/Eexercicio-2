1047b7d90efb13489555f7d1a120f1ad166ee399
henrique.tomaz <carlos.henrique.ext@synchro.com.br>
2024-03-17 16:29:56 -0300

    adiciona ajuste

--- 02.py
+++ 02.py
@@ -5,8 +5,9 @@ mostre o resultado da expressão da questão anterior.
 x = int(input("Digite o primeiro valor: "))
 y = int(input("Digite o segundo valor: "))
 z = int(input("Digite o terceito valor: "))
+w = 0
 
 if x*y < z/x or x / y < z * x and z * y < x:
-   print(True)
+   print("w = True")
 else: 
-  print(False)
+  print("w = False")
